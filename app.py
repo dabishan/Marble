ac13a225c74c8fd12966e70b8aa5368a438a9f82
Bishan <bishan.da@live.com>
2018-06-16 13:48:56 -0700

    App initialization for flask

---
+++ app.py
@@ -0,0 +1,16 @@
+#######################
+# Contain Flask and Initilization
+from flask import Flask
+import logging
+
+# Initialize Flask App
+app = Flask(__name__)
+app.config.from_pyfile('config.py')
+
+# Set up Logging
+file_handler = logging.FileHandler(app.config['LOG_FILE'])
+file_handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(message)s (%(funcName)s: %(lineno)d)'))
+file_handler.setLevel(app.config['DEBUG_LEVEL'])
+app.logger.addHandler(file_handler)
+
+
