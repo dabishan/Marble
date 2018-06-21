60ffa6d59415c3adf7d9e53501ba70c8e4e5186d
Bishan <bishan.da@live.com>
2018-06-20 23:12:16 -0700

    Moved logger initalization inside app context

--- app/app.py
+++ app/app.py
@@ -8,9 +8,10 @@ app = Flask(__name__)
 app.config.from_pyfile('config.py')
 
 # Set up Logging
-file_handler = logging.FileHandler(app.config['LOG_FILE'])
-file_handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(message)s (%(funcName)s: %(lineno)d)'))
-file_handler.setLevel(app.config['DEBUG_LEVEL'])
-app.logger.addHandler(file_handler)
+with app.app_context():
+    file_handler = logging.FileHandler(app.config['LOG_FILE'])
+    file_handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(message)s (%(funcName)s: %(lineno)d)'))
+    file_handler.setLevel(app.config['DEBUG_LEVEL'])
+    app.logger.addHandler(file_handler)
 
 
