24e35c4fefeb49cfe40202ad430ceeb834feed57
Bishan <bishan.da@live.com>
2018-07-30 19:39:04 -0700

    add differnt salt for test purposes

--- tests/models/fixtures.py
+++ tests/models/fixtures.py
@@ -5,5 +5,6 @@ import pytest
 @pytest.fixture(scope="module")
 def app_setup():
     app.config['DEFAULT_DB'] = 'tests'
+    app.config['SALT'] = "$2b$08$duvTxgV7EW9s98pmpFgRAO"
     with app.app_context():
         yield app
